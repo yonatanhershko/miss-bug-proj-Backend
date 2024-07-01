b1fb7993c62a900b8f14177b5c8cd294191155de
Yonatan Hershko <yonher8@gmail.com>
2024-07-01 18:25:36 +0300

    Add cookie

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { bugService } from './services/bug.service.js'
+import cookieParser from 'cookie-parser'
 import { loggerService } from './services/logger.service.js'
 // import hgj from "../"
 
-
 const app = express()
+app.use(cookieParser())
 app.use(express.static('../miss-bug-starter-react'))
 
 
@@ -31,8 +32,8 @@ app.get('/api/bug/save', (req, res) => {
     const bugToSave = {
         _id: req.query._id,
         title: req.query.title || '',
-        description: req.query.description || '', 
-        severity: +req.query.severity  || 0,
+        description: req.query.description || '',
+        severity: +req.query.severity || 0,
         createdAt: req.query.createdAt ? +req.query.createdAt : Date.now()
     }
     bugService.save(bugToSave)
@@ -43,12 +44,35 @@ app.get('/api/bug/save', (req, res) => {
         })
 })
 
+
+
+const visitedBugs = {}
+
 //write its without the :
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
-    console.log('bugId:', bugId)
+   
+    if (!visitedBugs[bugId]) {
+        visitedBugs[bugId] = 1
+    } else {
+        visitedBugs[bugId]++
+    }
+    console.log('Current visitedBugs state:',visitedBugs)
+   
+    if (visitedBugs[bugId] > 3) {
+        return res.status(401).send('Wait for a bit')
+    }
+
+    let totalVisits = req.cookies.visitedCount || 0
+    totalVisits++
+    res.cookie('visitedCount', totalVisits, { maxAge: 3 * 1000 })
+
+    // console.log('bugId:', bugId)
     bugService.getById(bugId)
-        .then(bug => res.send(bug))
+        .then(bug => {
+            res.send(bug)
+
+        })
         .catch((err) => {
             loggerService.error('Cannot get bug', err)
             res.status(500).send('Cannot get bug')
@@ -56,7 +80,6 @@ app.get('/api/bug/:bugId', (req, res) => {
 })
 
 app.get('/api/bug/:bugId/remove', (req, res) => {
-    // console.log('req.params:', req.params)
     const { bugId } = req.params
     bugService.remove(bugId)
         .then(() => res.send(`Bug (${bugId}) removed!`))
@@ -66,7 +89,6 @@ app.get('/api/bug/:bugId/remove', (req, res) => {
         })
 
 })
-
 
 
 app.listen(3030, () => console.log('Server ready at port 3030!'))
