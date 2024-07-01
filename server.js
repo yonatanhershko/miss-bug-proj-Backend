fe4b5df54c170ee2258fc841bfeadcd1515de9f5
Yonatan Hershko <yonher8@gmail.com>
2024-07-01 15:41:03 +0300

    add basic port data

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 
 const app = express()
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello There!')
 })
+
+app.get('/api/bug', (req, res) => {
+    const filterBy = {
+        title: req.query.title,
+        severity: +req.query.severity
+    }
+    bugService.query(filterBy)
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(500).send('Cannot get bugs')
+        })
+})
+
 
 
 
---
+++ services/bug.service.js
@@ -0,0 +1,64 @@
+import { utilService } from "./util.service.js"
+import fs from 'fs'
+
+const bugs = utilService.readJsonFile('data/bug.json')
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+function query(filterBy = {}) {
+    return Promise.resolve(bugs)
+        .then(bugs => {
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                bugs = bugs.filter(bug => regExp.test(bug.title))
+            }
+            if (filterBy.severity) {
+                bugs = bugs.filter(bug => bug.severity >= filterBy.severity)
+            }
+            return bugs
+        })
+}
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    if (!bug) return Promise.reject('Cannot find bug - ' + bugId)
+    return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const bugIdx = bugs.findIndex(bug => bug._id === bugId)
+    if (bugIdx < 0) return Promise.reject('Cannot find bug - ' + bugId)
+    bugs.splice(bugIdx, 1)
+    return _saveBugsToFile().then(() => `bug (${bugId}) removed!`)
+}
+
+
+function save(bugToSave) {
+    if (bugToSave._id) {
+        const bugIdx = bugs.findIndex(bug => bug._id === bugToSave._id)
+        bugs[bugIdx] = bugToSave
+    } else {
+        bugToSave._id = utilService.makeId()
+        bugs.unshift(bugToSave)
+    }
+
+    return _saveBugsToFile().then(() => bugToSave)
+}
+
+
+function _saveBugsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/car.json', data, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
