e3eadfe4cbd578b781097c76c3cd1012c9a745ae
Yonatan Hershko <yonher8@gmail.com>
2024-07-01 16:03:51 +0300

    add basic crud work

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get('/api/bug/save', (req, res) => {
         severity: +req.query.severity  || 0,
         createdAt: req.query.createdAt ? +req.query.createdAt : Date.now()
     }
-
     bugService.save(bugToSave)
         .then(bug => res.send(bug))
         .catch((err) => {
@@ -42,7 +41,29 @@ app.get('/api/bug/save', (req, res) => {
         })
 })
 
+//write its without the :
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    console.log('bugId:', bugId)
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch((err) => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
 
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    // console.log('req.params:', req.params)
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(`Bug (${bugId}) removed!`))
+        .catch((err) => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug', err)
+        })
+
+})
 
 
 
